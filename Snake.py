33ac3ee832600ba0c3d13503918ebf256fab8acc
Bruno Pigari Rateiro <105450191+BPRateiro@users.noreply.github.com>
2022-06-03 09:53:09 -0300

    Snake and snack on screen.

--- Snake.py
+++ Snake.py
@@ -15,7 +15,8 @@ COLORS = {'black': (0, 0, 0),
           'white': (255, 255, 255),
           'red': (255, 0, 0),
           'green': (0, 255, 0),
-          'blue': (0, 0, 255)}
+          'blue': (0, 0, 255),
+          'yellow': (255, 211, 67)}
 # Defining some window parameters:
 SIZE = (500, 500)
 ROWS = 20
@@ -23,7 +24,7 @@ ROWS = 20
 
 # Classes Definition
 class Cube(object):
-    def __init__(self, position, x_direction=1, y_direction=0, color=COLORS['red']):
+    def __init__(self, position, x_direction=1, y_direction=0, color=COLORS['yellow']):
         self.position = position
         self.x_direction = x_direction
         self.y_direction = y_direction
@@ -32,21 +33,21 @@ class Cube(object):
     def move(self, x_direction, y_direction):
         self.x_direction = x_direction
         self.y_direction = y_direction
-        self.position += self.x_direction, self.y_direction
+        self.position = self.position[0] + self.x_direction, self.position[1] + self.y_direction
 
     def draw(self, surface, eyes=False):
         distance = SIZE[0] // ROWS
         i = self.position[0]
         j = self.position[1]
 
-        square = (i*distance+1, j*distance+1, distance-2, distance-2)
+        square = (i * distance + 1, j * distance + 1, distance - 2, distance - 2)
         pygame.draw.rect(surface, self.color, square)
 
         if eyes:
-            square_center = distance//2
+            square_center = distance // 2
             radius = 3
-            circle_center_1 = (i*distance+square_center-radius, j*distance+8)
-            circle_center_2 = (i*distance+distance-radius*2, j*distance+8)
+            circle_center_1 = (i * distance + square_center - radius, j * distance + 8)
+            circle_center_2 = (i * distance + distance - radius * 2, j * distance + 8)
             pygame.draw.circle(surface, COLORS['black'], circle_center_1, radius)
             pygame.draw.circle(surface, COLORS['black'], circle_center_2, radius)
 
@@ -57,7 +58,7 @@ class Snake(object):
 
     def __init__(self, color, position):
         self.color = color
-        self.head = Cube(position)
+        self.head = Cube(position, color=self.color)
         self.body.append(self.head)
         self.x_direction = 0
         self.y_direction = 1
@@ -79,10 +80,10 @@ class Snake(object):
                     self.y_direction = 0
                 elif keys[pygame.K_UP]:
                     self.x_direction = 0
-                    self.y_direction = 1
+                    self.y_direction = -1
                 elif keys[pygame.K_DOWN]:
                     self.x_direction = 0
-                    self.y_direction = -1
+                    self.y_direction = 1
                 else:  # If other buttons were pressed, no need to remember the snake position/direction
                     break
                 self.turns[self.head.position[:]] = (self.x_direction, self.y_direction)
@@ -117,6 +118,23 @@ class Snake(object):
             else:
                 cube.draw(surface)
 
+    def add_cube(self):
+        """Adds a cube to the tail when snake gets the snack"""
+        tail = self.body[-1]
+        x_tail_direction, y_tail_direction = tail.x_direction, tail.y_direction
+
+        if x_tail_direction == 1 and y_tail_direction == 0:
+            self.body.append(Cube((tail.position[0] - 1, tail.position[1]), color=self.color))
+        elif x_tail_direction == -1 and y_tail_direction == 0:
+            self.body.append(Cube((tail.position[0] + 1, tail.position[1]), color=self.color))
+        elif x_tail_direction == 0 and y_tail_direction == 1:
+            self.body.append(Cube((tail.position[0], tail.position[1] - 1), color=self.color))
+        elif x_tail_direction == 0 and y_tail_direction == -1:
+            self.body.append(Cube((tail.position[0], tail.position[1] + 1), color=self.color))
+
+        self.body[-1].x_direction = x_tail_direction
+        self.body[-1].y_direction = y_tail_direction
+
 
 # Functions Definition
 def draw_grid(surface):
@@ -133,12 +151,29 @@ def draw_grid(surface):
         pygame.draw.line(surface, COLORS['white'], (0, y), (width, y))
 
 
-def redraw_window(surface: pygame.Surface, snake: Snake):
+def redraw_window(surface: pygame.Surface, snake: Snake, snack: Cube):
     """Erases the window, draws the grid and updates the visual"""
     surface.fill(COLORS['black'])
     draw_grid(surface)
     snake.draw(surface)
+    snack.draw(surface)
     pygame.display.update()
+
+
+def random_snack(snake):
+    """Generates random snacks, possible everywhere but where the snake body is."""
+    occupied_positions = snake.body
+
+    while True:
+        x = random.randrange(ROWS)
+        y = random.randrange(ROWS)
+
+        if len(list(filter(lambda cb: cb.position == (x, y), occupied_positions))) > 0:
+            continue
+        else:
+            break
+
+    return x, y
 
 
 def main():
@@ -146,6 +181,7 @@ def main():
     window = pygame.display.set_mode(SIZE)
     clock = pygame.time.Clock()
     snake = Snake(COLORS['blue'], (ROWS // 2, ROWS // 2))  # RGB values, coordinates
+    snack = Cube(random_snack(snake))
 
     pygame.init()
 
@@ -154,7 +190,10 @@ def main():
         clock.tick(10)  # Speed in FPS
 
         snake.move()
-        redraw_window(window, snake)
+        if snake.body[0].position == snack.position:  # If the snake's head is at the snacks position:
+            snake.add_cube()
+            snack = Cube(random_snack(snake))
+        redraw_window(window, snake, snack)
 
 
 # Good luck, have fun!
